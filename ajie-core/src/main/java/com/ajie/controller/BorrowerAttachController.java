8c36a5e26534f89c254c231678749dea2d47b25b
阿劼 <smemoryouoth@yeah.net>
2021-07-22 17:07:47 +0800

    123

--- ajie-core/src/main/java/com/ajie/controller/BorrowerAttachController.java
+++ ajie-core/src/main/java/com/ajie/controller/BorrowerAttachController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/borrowerAttach")
 public class BorrowerAttachController {
     public static void main(String[] args) {
-        System.out.println(123);
+        System.out.println(1230);
     }
 }
 
